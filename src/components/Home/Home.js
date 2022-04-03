5f59260099fa59fc72d4fdbc11304118da354c5a
abidhossain2 <tanvirabid229@gmail.com>
2022-04-04 00:57:18 +0600

    navlink include

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            
+        <>
+        <div className='home-container'>
+            <h1><span className='first-heading'>Comfortable chair</span> <br /> <span className='second-heading'>for your interior</span></h1>
+            <img src="https://images.unsplash.com/photo-1506898667547-42e22a46e125?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNoYWlyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
         </div>
+        <div>
+            <h1>Buyer Review</h1>
+        </div>
+        </>
     );
 };
 
