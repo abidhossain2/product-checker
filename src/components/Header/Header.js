c14e5ec6e90769e85742b74ba2a33be40cb804e7
abidhossain2 <tanvirabid229@gmail.com>
2022-04-04 00:42:29 +0600

    header set

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='nav-link'>
+            <CustomLink className='link' to='/'>Home</CustomLink>
+            <CustomLink className='link' to='/review'>Review</CustomLink>
+            <CustomLink className='link' to='/dashboard'>Dashboard</CustomLink>
+            <CustomLink className='link' to='/blog'>Blog</CustomLink>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <img src="https://images.unsplash.com/photo-1594322436404-5a0526db4d13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8NDA0JTIwZXJyb3J8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
