dac3c604ab7c90f0a64ac7fe0c36d54e6f3c4e18
abidhossain2 <tanvirabid229@gmail.com>
2022-04-04 01:00:04 +0600

    home page set

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <h1><span className='first-heading'>Comfortable chair</span> <br /> <span className='second-heading'>for your interior</span></h1>
             <img src="https://images.unsplash.com/photo-1506898667547-42e22a46e125?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNoYWlyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
         </div>
-        <div>
+        <div className='review-container'>
             <h1>Buyer Review</h1>
         </div>
         </>
