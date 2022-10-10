6d35cd890bef21701df065b2c254b21a469d3bf9
abidhossain2 <tanvirabid229@gmail.com>
2022-10-10 12:03:50 +0600

    image added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
             <div className='heading'><span className='first-heading'>Comfortable chair</span> <br /> <span className='second-heading'>for your interior</span>
             <p>A stunning wingback shape and ottoman give <br /> this charcoal reading chair the edge over competition.</p>
             </div>
-            <img src="http://cdn.home-designing.com/wp-content/uploads/2016/12/grey-wingback-and-ottoman-cool-reading-chairs-600x479.jpg" alt="" />
+            <img src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZnVybml0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
         </div>
             <h1>Buyer Review</h1>
         <div className='review-container'>
